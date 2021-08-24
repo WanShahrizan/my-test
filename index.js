309b115d00fc310a223b3fd15e995c40dcb5cdb5
WanShahrizan <shahrizan_98@yahoo.com>
2021-08-24 14:29:39 +0800

    Uploading.

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("This Works");
+
+// Working with APIs
+// We need to answer the following questions
+// - What is the endpoint (URL)?
+// - What query string/parameters do we need to provide?
+//      - What are required parameters?
+//      - What optional parameters do we want to add?
